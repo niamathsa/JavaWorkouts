8724f01f389b993bcbb9389b6c9ebf0ab5bc54d8
syed.openwave <niamathsa@gmail.com>
2022-01-25 12:52:45 +0530

    best commit

---
+++ ExerciseOne.java
@@ -0,0 +1,18 @@
+class ExerciseOne {
+
+ 
+ public static void main (String [] args)
+{
+     int sum1 = sum(2,3);
+	int sum2 = sum(4,9);
+
+  System.out.println(sum1);
+}
+
+   private static int sum (int a, int b){
+
+      return a+b;
+
+
+}
+}
---
+++ MethodExe1.java
@@ -0,0 +1,18 @@
+class MethodExe1 {
+
+ 
+ public static void main (string [] args)
+{
+     int sum1 = add(2,3);
+	int sum2 = add(4,9);
+
+  System.out.println(sum1);
+}
+
+   privat static int sum (int a, int b){
+
+      return a+b;
+
+
+}
+}
