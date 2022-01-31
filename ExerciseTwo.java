7debfcfa95f863c4504e77e545d3944339d6b4d8
syed.openwave <niamathsa@gmail.com>
2022-01-31 17:30:47 +0530

    update

---
+++ ExerciseTwo.java
@@ -0,0 +1,28 @@
+class ExerciseTwo {
+
+ 
+ public static void main (String [] args)
+{
+     isEven(2);
+     
+
+     isEven(7);
+
+  isEven(5);
+}
+
+   private static void isEven (int a){
+   
+  
+   if (a-2 ==0)
+
+System.out.println("This is Even");
+
+  else 
+
+System.out.println("This is Odd");
+
+      
+
+}
+}
